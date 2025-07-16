fcceaa80849508a2f1083cf9e9f599d96fd3bdda
DEV43-78 <85327312+DEV43-78@users.noreply.github.com>
2025-07-16 10:25:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 
-const firebaseConfig = {
+// config.js
+export const firebaseConfig = {
   apiKey: "AIzaSyAt5cUIs3fCKigH0aRZvPWBKMaxkUOH1Zk",
   authDomain: "link-shortner-project-2aed0.firebaseapp.com",
   databaseURL: "https://link-shortner-project-2aed0-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "link-shortner-project-2aed0",
-  storageBucket: "link-shortner-project-2aed0.firebasestorage.app",
+  storageBucket: "link-shortner-project-2aed0.appspot.com",
   messagingSenderId: "984742490311",
   appId: "1:984742490311:web:90c0b870b690b7dd558ee2",
   measurementId: "G-P70DH8GPKC"
 };
-
