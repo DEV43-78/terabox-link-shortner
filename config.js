fe54a6d7f3beaf60f0449b2ba9b3f17509a7f35b
DEV43-78 <85327312+DEV43-78@users.noreply.github.com>
2025-07-16 10:22:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,12 @@
-{
-  "type": "service_account",
-  "project_id": "link-shortner-project-2aed0",
-  "private_key_id": "06e95a24807bb1c8dc142d188e6265e14c23efb8",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCz0HAMt6fE3531\nh4D+TUYUHZd3RyXVMa4yQylygcZCo/j5BTl6APtoUw0N3aMbZr+8sthOYzQNw24o\naRo6ORlMeGoTBp1zrAdN5LrzcRFnF90WD/DWpAG8g97lrGRlFe0dXicix46NHvg4\nPhJvh0RImPMe7fV9iEdcqfA1tNwthf8K6FK8sU2k6H17uy+lyWPc+P1xkxegepNW\n9nG0YnkvIrdSarJlBW59qrqjLoSDlaUlSPrUhkVeVLj4dIQdYl5xpkfAMxrPXl2o\nWstRIN5B6IS1XWteAOJXHJCUu8LeWmsI8UAHNSJgKCcwbnRlzjvLMoPAoeMkqOs+\nz2HGzA27AgMBAAECggEATM5QRkZeXZws0xc1tKoaMHZxX8wer2GAqEyC1Dv1jTqQ\nGwItylzIBohPD6inxokNhlMYTOdX8TcUgyNdxhfkpALdQFI3iBSnrBBzAJA/4DrT\nv1xAwdYdPcJQP+dynLEQbtZsPhFiZQog+Zkd89CiDKZ0VrZLAXtoC/80sSQXVkPp\nw2AaaxU87n84nBlMecTshWEMZ72vaOdNRZA3Bb6lThGEl+ng6SuC5K3PUu4vjn8p\nazkc+W3ymo5EpxoiDc6tY7MbYdpqwp3pOXo2bWzxy2s1+deZ09QKylaK8pCxRjbh\n4FJpxtO3RSrdeGfJh4Ovgbd6cBk1RNmms0qafxoiTQKBgQDo8KQugHVYH2cvP8+O\nII+jH151jgojrCHDa+qyaVecyv84JluvWsOApK7BMoat23K0QsAd9EcaA9ei0378\nhB2kTQ14ubAdObXKXjifG+a47VNZZPUH6eRnbIjl7CaoLEa0/AIT7YOwGiGaLAKA\nDZS7BAk68h0/um+/M2HLj+JNhwKBgQDFnXA8Z112KWsXaodWVsLIf1SL12O9muSz\niuHI4vdouTdNpelDVC6DgzmxniWxHEMTwqVR9pyMZmRY8MQeMIwUOzU5Q1Y3OHSz\nr8tS3MvdT3PKfbrGKRnKWcgDs8P1xgsrPKxI9ITU6rwN+QUm2qqprzJtj0d8sKLF\n81sPnLtrLQKBgQDRp9V97SywlgMfKcW73F6rqQ4xrBhAJyt0YmspDVtHygWQGiYn\nW//eh1qqlLlXPc6to+6ah0R/i7nH2afPy3GKcbeQU0DJ7SlL7SX1Pv2YsDJ2oUFR\nUBi9ZZiJqNTaRI+XUcLlAL+n/r0DsFzHuHGHKYjNPyFIPJtbcfmxZ919fQKBgCnt\nko+uWr0kYo1wU9C0zdUedwDIrWfgQl/5upgV1+qHSb2W0xwu80OCBy6xYEvotC6w\nIMV4LpT3cJkSVHoXUNFGEq/YoqVKvDAXv2sQX8zP+8Bd1tAOjmNJk099x7kQ0PPW\nhrNf9gL63vWT/UzWHlmsS2XFfaRuEcswLATu+zDtAoGBAMekcny3QMD4BwklWFnv\n/GAus7ni+M21vMHfE210T+JYhTL3mmbiC8K6JuqDFF+sLE9V8G1907s5jfIobTpy\nkomiOEypGslslqgDhEyYjZHtnjV1hhiO0tNx80gCxPJ50LmrEz+R49UAqzUidqK2\n/ealqvmiMSiYQxFGaemTEpv2\n-----END PRIVATE KEY-----\n",
-  "client_email": "firebase-adminsdk-fbsvc@link-shortner-project-2aed0.iam.gserviceaccount.com",
-  "client_id": "102132603471155807553",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40link-shortner-project-2aed0.iam.gserviceaccount.com",
 
-}
+const firebaseConfig = {
+  apiKey: "AIzaSyAt5cUIs3fCKigH0aRZvPWBKMaxkUOH1Zk",
+  authDomain: "link-shortner-project-2aed0.firebaseapp.com",
+  databaseURL: "https://link-shortner-project-2aed0-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "link-shortner-project-2aed0",
+  storageBucket: "link-shortner-project-2aed0.firebasestorage.app",
+  messagingSenderId: "984742490311",
+  appId: "1:984742490311:web:90c0b870b690b7dd558ee2",
+  measurementId: "G-P70DH8GPKC"
+};
+
